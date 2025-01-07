0956da7274044c75ff488da20fb1a058122914e3
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2025-01-07 21:52:46 +0900

    Title: 1, 2, 5 더하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250107/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        dp = new int[n + 1];
+        dp[0] = 1; // 마지막에 등장하는 수를 고정했을 경우, 
+                   // k가 되는 수가 등장했을 경우의 수 1
+        for (int i = 1; i <= n; i++) {
+            // i가 1보다 클 경우 
+            // 마지막에 1을 고정한 경우의 수를 더한다.
+            if (i >= 1) {
+                dp[i] += dp[i - 1];
+            }
+
+            // i가 2보다 클 경우
+            // 마지막에 2를 고정한 경우의 수를 더한다.
+            if (i >= 2) {
+                dp[i] += dp[i - 2];
+            }
+
+            // i가 5보다 클 경우
+            // 마지막에 5를 고정한 경우의 수를 더한다.
+            if (i >= 5) {
+                dp[i] += dp[i - 5];
+            }
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
